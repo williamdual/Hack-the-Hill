4b2ac0c6976d2829724996bcc585711080841cea
williamdual <williamdual115@gmail.com>
2023-03-04 15:15:23 -0500

    Basic Player Movement

---
+++ sewerwoman/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
