5bfc4c18765d0d9de4a3da937c82577d47decd6f
UtterlyWu <71282856+UtterlyWu@users.noreply.github.com>
2023-03-04 17:49:35 -0500

    Initial
    Initial

--- sewerwoman/Assets/Enemies/MutantFish.cs
+++ sewerwoman/Assets/Enemies/MutantFish.cs
@@ -14,14 +14,31 @@ public class MutantFish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("afterThrowFish");
         velocity = new Vector2(0, 0);
         m_kbody = GetComponent<Rigidbody2D>();
     }
 
     void move() {
         Vector2 targetPos = target.transform.position;
-        velocity = (targetPos - (Vector2)transform.position).normalized;
-        m_kbody.MovePosition(m_kbody.position + velocity * speed * Time.fixedDeltaTime);
+        Vector2 direction = (targetPos - (Vector2)transform.position).normalized;
+        velocity = direction * speed * Time.fixedDeltaTime;
+        m_kbody.MovePosition(m_kbody.position + velocity);
+        m_kbody.MoveRotation(Quaternion.LookRotation(direction));
+    }
+
+    public void throwFish(Vector2 direction, int force) {
+
+    }
+
+    private IEnumerator afterThrowFish(){
+        yield return new WaitForSeconds(3);
+        Debug.Log("hello");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collision");
     }
 
     // Update is called once per frame
