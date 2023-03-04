0cf92ad3bd06b7be68c8aa4d34dca8be4d83ae9e
T0mGates <vitdufour@gmail.com>
2023-03-04 16:16:54 -0500

    Radioactive Shader
    implemented shader for radioactivity

---
+++ sewerwoman/Assets/IdleRadioactive.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleRadioactive : MonoBehaviour
+{
+
+    private Material material;
+    float t = 0;
+    bool decreasing = false;
+    float radioactivity = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t >= 1)
+        {
+            decreasing = true;
+        }
+        
+        if (decreasing)
+        {
+            t -= Time.deltaTime;
+        }
+        else
+        {
+            t += Time.deltaTime;
+        }
+
+        if(t <= 0 && decreasing)
+        {
+            decreasing = false;
+        }
+
+        radioactivity = Mathf.Lerp(0, 1, t);
+        material.SetFloat("_RadioactiveAmount", radioactivity);
+    }
+}
