1afa4aebc2216590e8631f053b0107f646e7e384
williamdual <williamdual115@gmail.com>
2023-03-05 03:29:40 -0500

    Added audio and audio manager

---
+++ sewerwoman/Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    public AudioSource AudioPlayer;
+
+    public AudioClip[] BeamCharge;
+    public AudioClip[] BeamFire;
+    public AudioClip[] Garbage;
+    public AudioClip[] FishHappy;
+    public AudioClip[] FishHit;
+    public AudioClip[] PlayerDamage;
+    public AudioClip[] Victory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(string name)
+    {
+        AudioClip[] selected;
+        switch (name)
+        {
+            case "BeamCharge":
+                selected = BeamCharge; break;
+            case "BeamFire":
+                selected = BeamFire; break;
+            case "Garbage":
+                selected = Garbage; break;
+            case "FishHappy":
+                selected = FishHappy; break;
+            case "FishHit":
+                selected = FishHit; break;
+            case "PlayerDamage":
+                selected = PlayerDamage; break;
+            case "Victory":
+                selected = Victory; break;
+            default:
+                selected = Victory;
+                Debug.Log("Sound not found"); break;
+        }
+        AudioPlayer.clip = selected[Random.Range(0, selected.Count()-1)];
+        if (!AudioPlayer.isPlaying)
+        {
+            AudioPlayer.Play();
+        }
+    }
+}
